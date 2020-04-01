a38f83b319a75ebddf8b2c7107b7da83d0e47b27
jopejor <lionzord@gmail.com>
2020-03-31 22:50:38 -0700

    Add files via upload
    "ggplot2 theme"

--- src/r/eefe_functions.R
+++ src/r/eefe_functions.R
@@ -1,3 +1,33 @@
+# Theme
+theme_xp <- function () { 
+  theme_minimal(base_size=14, base_family="") %+replace%
+    theme(
+      plot.title = element_text(face = "bold",
+                                size = rel(1.2), hjust = 0.5),
+      text = element_text(),
+      panel.background = element_rect(colour = NA),
+      #plot.background = element_rect(colour = NA),
+      # panel.border = element_rect(colour = NA),
+      axis.title = element_text(face = "bold",size = rel(1)),
+      axis.title.y = element_text(angle=90,vjust =2),
+      axis.title.x = element_text(vjust = -0.2),
+      axis.text = element_text(), 
+      axis.line = element_line(colour="black"),
+      axis.ticks = element_line(),
+      panel.grid.major = element_line(colour="#f0f0f0"),
+      panel.grid.minor = element_blank(),
+      legend.key = element_rect(colour = NA),
+      legend.position = "bottom",
+      legend.direction = "horizontal",
+      legend.key.size= unit(0.2, "cm"),
+      legend.margin = unit(0, "cm"),
+      legend.title = element_text(face="italic"),
+      plot.margin=unit(c(10,5,5,5),"mm"),
+      strip.background=element_rect(colour="#f0f0f0",fill="#f0f0f0"),
+      strip.text = element_text(face="bold")
+    )
+}
+
 # Color mutations
 alter_fun = list(
   background = function(x, y, w, h) {
